7f92c36ba6417f2fd31970defbb230904542a819
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-14 16:38:10 -0400

    add heroku-builds plugin for deployment

--- build_script.bat
+++ build_script.bat
@@ -13,5 +13,6 @@ cd IdaBWellsProject\IdaBWellsProject
 pip install datasette sqlite-utils heroku
 
 sqlite-utils insert red_record.db red-record-data red-record-states.geojson
+heroku plugins:install heroku-builds
 datasette install datasette-cluster-map
 datasette publish heroku --name "redrecord" --install=datasette-cluster-map

da7b7f6219acd8e716ca1ad1b1263e08644a6c41
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-14 17:45:13 -0400

    split set up and build

--- build_script.sh
+++ build_script.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-echo pwd
-
-pip install datasette sqlite-utils heroku
-
-(
-echo "dec11rll@gmail.com"
-echo "Howard-Deploy"
-) | heroku login -i
 
 sqlite-utils insert red_record.db red-record-data red-record-states.geojson
 heroku plugins:install heroku-builds
---
+++ set-up.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pip install datasette sqlite-utils heroku
+
+(
+echo "dec11rll@gmail.com"
+echo "Howard-Deploy"
+) | heroku login -i

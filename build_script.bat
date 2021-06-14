eb597227b5dba18343ac290aa8b67212cec9dc74
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-14 15:21:16 -0400

    attempting real batch file for publishing updated db to Datasette

--- build_script.bat
+++ build_script.bat
@@ -1,1 +1,15 @@
-%cd%
+cd ..\..
+echo "machine api.heroku.com
+  login dec11rll@gmail.com
+  password 5d12a537-614a-4375-af23-b7cab9f931f5
+machine git.heroku.com
+  login dec11rll@gmail.com
+  password 5d12a537-614a-4375-af23-b7cab9f931f5" >> _netrc
+
+cd IdaBWellsProject\IdaBWellsProject
+
+pip install datasette sqlite-utils heroku
+
+sqlite-utils insert red_record.db red-record-data red-record-states.geojson
+datasette install datasette-cluster-map
+datasette publish heroku --name "redrecord" --install=datasette-cluster-map

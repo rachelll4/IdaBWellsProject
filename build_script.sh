b2dab85f224c0a07a0afd745f04bbc2dd19ca2b5
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-14 17:36:24 -0400

    auto heroku login

--- build_script.sh
+++ build_script.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 echo pwd
-cd ~
-(
-echo machine api.heroku.com
-echo   login dec11rll@gmail.com
-echo   password 5d12a537-614a-4375-af23-b7cab9f931f5
-echo machine git.heroku.com
-echo   login dec11rll@gmail.com
-echo   password 5d12a537-614a-4375-af23-b7cab9f931f5 
-)>> .netrc
-
-cd runner/work/IdaBWellsProject/IdaBWellsProject
 
 pip install datasette sqlite-utils heroku
+
+(
+echo dec11rll@gmail.com
+echo Howard-Deploy
+) | heroku login
 
 sqlite-utils insert red_record.db red-record-data red-record-states.geojson
 heroku plugins:install heroku-builds

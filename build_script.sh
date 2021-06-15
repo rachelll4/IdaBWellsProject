00007f04b2f17ed78728bee2843a48486dc14ea3
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-15 15:17:50 -0400

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+pip install datasette sqlite-utils heroku
+cd ~
+sudo cat >> .netrc
+sudo echo "machine api.heroku.com
+  login dec11rll@gmail.com
+  password ece9d5cb-78ed-4075-a51e-b9fd6d1e6c08
+machine git.heroku.com
+  login dec11rll@gmail.com
+  password ece9d5cb-78ed-4075-a51e-b9fd6d1e6c08" >> .netrc
+cat .netrc
+
+cd runner/work/IdaBWellsProject/IdaBWellsProject
+
 sqlite-utils insert red_record.db red-record-data red-record-states.geojson
 heroku plugins:install heroku-builds
 datasette install datasette-cluster-map

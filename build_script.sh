870cb7e5897741364593328816c3ab8f9230c132
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-16 11:35:26 -0400

    edit directory name to match project

--- build_script.sh
+++ build_script.sh
@@ -10,7 +10,7 @@ machine git.heroku.com
   login dec11rll@gmail.com
   password ece9d5cb-78ed-4075-a51e-b9fd6d1e6c08" >> .netrc
 
-cd /home/runner/work/IdaBWellsProject/IdaBWellsProject
+cd /home/runner/work/Lynching_Web_App/Lynching_Web_App
 
 sqlite-utils insert red_record.db red_record red_record_states.csv --csv
 heroku plugins:install heroku-builds

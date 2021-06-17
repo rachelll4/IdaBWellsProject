be00b1d56accb160eadca02deaff722412f64a41
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2021-06-17 11:21:49 -0400

    added filtering on amended json structure

--- website/script.js
+++ website/script.js
@@ -7,7 +7,9 @@
 
     
     //store JSON in a variable
-    let requestURL = 'https://redrecord.herokuapp.com/red_record/red_record.json?'; //_sort=alleged_crime&state__exact=Texas';
+    let requestURL = 'https://redrecord.herokuapp.com/red_record/red_record.json?_size=500';//&state__exact=Texas';
+
+//https://redrecord.herokuapp.com/red_record?sql=select+rowid%2C+id%2C+names%2C+city%2C+state%2C+date%2C+alleged_crime%2C+full_location%2C+longitude%2C+latitude+from+red_record+limit+101&p0=n%25
 
     //create request object
     let request = new XMLHttpRequest();
@@ -26,11 +28,25 @@
     request.onload = function() {
         const murder = request.response; 
         console.log(murder);
-        populateDatatable(murder);
         transform(murder);  
+        populateDatatable();
+        
     }
 
-    //END REQUEST
+    /*END REQUEST
+
+    class maipulate{
+
+        function select(data[]){
+
+        }
+
+        function filter(data[]){
+
+        }
+
+    }*/
+
 
     /*
     filter function
@@ -58,36 +74,41 @@
     }
 
     //Create function to populate header based on JSON
-    function populateDatatable(obj) {       
-
-        const victims = obj['rows'];
+    function populateDatatable() {       
     
-        for (let i = 0; i < victims.length; i++) {
-            const myArticle = document.createElement('article');
-            const myH2 = document.createElement('h2');
-            const myPara1 = document.createElement('p');
-            const myPara2 = document.createElement('p');
-            const myPara3 = document.createElement('p');
-            const myPara4 = document.createElement('p');
+        for (let i = 0; i < victimDataArray.length; i++) {
+
+            if ({victimDataArray[i].alleged_crime =="Arson"){//true)
+
+                const myArticle = document.createElement('article');
+                const myH2 = document.createElement('h2');
+                const myPara1 = document.createElement('p');
+                const myPara2 = document.createElement('p');
+                const myPara3 = document.createElement('p');
+                const myPara4 = document.createElement('p');
+                //const myPara5 = document.createElement('p');
 
 
-            //if this was a string, say name, it would be:
-            //myH2.textContent = victims[i].name;
-            // EACH OF THESE SHOULD BE A BOOTSTRAP MODULE
+                //if this was a string, say name, it would be:
+                //myH2.textContent = victims[i].name;
+                // EACH OF THESE SHOULD BE A BOOTSTRAP MODULE
 
-            myH2.innerHTML = '<a href="http://www.umd.edu">Name: ' + victims[i]["2"] + '</a>';
-            myPara1.innerHTML = 'City: ' + victims[i]["3"];
-            myPara2.textContent = 'State: ' + victims[i]["4"];
-            myPara3.textContent = 'Date: ' + victims[i]["5"];
-            myPara4.innerHTML = '<strong>Crime: ' + victims[i]["6"] + '</strong>';
 
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myArticle.appendChild(myPara3);
-            myArticle.appendChild(myPara4);
+                myH2.innerHTML = '<a href="http://www.umd.edu">Name: ' + victimDataArray[i].names + '</a>';
+                myPara1.textContent = 'City: ' + victimDataArray[i].city;
+                myPara2.textContent = 'State: ' + victimDataArray[i].state;
+                myPara3.textContent = 'Date: ' + victimDataArray[i].date;
+                myPara4.innerHTML = '<strong>Crime: ' + victimDataArray[i].alleged_crime + '</strong>';
+                //myPara5.textContent = 'Row ID: ' +victimDataArray[i].rowid;
+                myArticle.appendChild(myH2);
+                myArticle.appendChild(myPara1);
+                myArticle.appendChild(myPara2);
+                myArticle.appendChild(myPara3);
+                myArticle.appendChild(myPara4);
+                //myArticle.appendChild(myPara5);
 
-            datatable.appendChild(myArticle);
+                datatable.appendChild(myArticle);
+            }
         }
 
         return false
